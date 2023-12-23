22ae0da1b1d6d567e7dfe052a3e87e7444b5433b
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-12-23 18:06:22 +0900

    feat: add basic structure of top page

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,91 @@
-import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import logo from "./img/logo.png";
+import manyPlatypus from "./img/many-platypus-without-background.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="text-9xl">Platypus</div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+    <div id="container" className="">
+      <header className="sticky top-0 z-10 shadow-md">
+        <div
+          id="title_logo"
+          className="flex flex-row gap-3 items-center px-8 py-3"
+        >
+          <a href="https://blog.platypuscode.com/" target="_blank">
+            <img
+              src={logo}
+              id="logo"
+              className="object-contain"
+              alt="Platypus Code logo"
+            />
+          </a>
+          <h1 id="title">Platypus Code</h1>
+        </div>
+        <div>
+          <nav>
+            <ul className="flex flex-row gap-10 justify-center">
+              <li className="p-3">
+                <a href="https://blog.platypuscode.com/" target="_blank">
+                  Portfolio
+                </a>
+              </li>
+              <li className="p-3">
+                <a href="https://blog.platypuscode.com/" target="_blank">
+                  Blog
+                </a>
+              </li>
+              <li className="p-3">
+                <a href="https://blog.platypuscode.com/about/" target="_blank">
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <div className="p-8">
+          <div id="mainvisual" className="flex flex-col items-center">
+            <img
+              src={manyPlatypus}
+              id="manyPlatypus"
+              alt="many Platypus in a box"
+              className="my-5 p-3"
+            />
+          </div>
+          <section id="portfolio" className="wrapper">
+            <h2 className="section-title">Portfolio</h2>
 
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+            <ul>
+              <li className="portfolio-item">
+                <img src={manyPlatypus} alt="テキストテキストテキスト"></img>
+                <h3 className="content-title">タイトルタイトル</h3>
+                <p>テキストテキストテキスト</p>
+              </li>
+              <li className="portfolio-item">
+                <img src={manyPlatypus} alt="テキストテキストテキスト"></img>
+                <h3 className="content-title">タイトルタイトル</h3>
+                <p>テキストテキストテキスト</p>
+              </li>
+              <li className="portfolio-item">
+                <img src={manyPlatypus} alt="テキストテキストテキスト"></img>
+                <h3 className="content-title">タイトルタイトル</h3>
+                <p>テキストテキストテキスト</p>
+              </li>
+            </ul>
+          </section>
+          <section id="about" className="wrapper">
+            <h2 className="section-title">About</h2>
+            <div id="about-text" className="text-3xl px-10">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+              nihil? Natus laborum aliquid possimus explicabo culpa a non ipsum
+              necessitatibus tempora minus fugit vel exercitationem labore,
+              maiores quasi, in sunt?
+            </div>
+          </section>
+        </div>
+      </main>
+      <footer id="footer">©︎Platypus Code</footer>
+    </div>
   );
 }
 
